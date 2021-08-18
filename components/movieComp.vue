ac3eceee15b59d5f8e2913263dd2a163364b2d4c
Cementator <46320092+Cementator@users.noreply.github.com>
2021-08-18 18:39:24 +0200

    gg

--- components/movieComp.vue
+++ components/movieComp.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col bg-gray-100 p-2">
-      <h1 class="text-xl">{{movieTitle}}</h1>
-      <h1 v-if="$store.state.movies.map(x => x.id).indexOf(movieTitle)"> in your wishlist</h1>
+      <h1 v-if="$store.state.movies.map(x => x.id).indexOf(movieId)>=0"> in your wishlist</h1>
+      <h1 else class="text-xl">{{movieTitle}}</h1>
       <img :src="'https://image.tmdb.org/t/p/w300'+baseImgPath" alt="Image">
       <nuxt-link :to="`/movies/${movieId}`" >Details</nuxt-link>
   </div>
